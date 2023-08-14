fb996cfcd73b10282b8c74ff8be44da678852dea
Berador Stambekov <stambekovbera@gmail.com>
2023-08-15 01:31:07 +0500

    fix not found page & fix mobile adaptive

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './NotFoundPage.module.scss';
 import cn from 'classnames';
+import { Link } from 'react-router-dom';
 
 interface INotFoundPageProps {
     className?: string;
@@ -9,8 +10,11 @@ interface INotFoundPageProps {
 export const NotFoundPage: React.FC<INotFoundPageProps> = (props) => {
     const { className } = props;
     return (
-        <div className={ cn(classes.notFoundPage, {}, [ className ]) }>
-            404
+        <div className={ cn( classes.notFoundPage, {}, [ className ] ) }>
+            Такой страницы не существует
+            <Link to='/'>
+                Главная
+            </Link>
         </div>
     );
 };
